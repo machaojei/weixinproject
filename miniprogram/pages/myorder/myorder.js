d767d767ff49403a0f8e684138d7054dacc5a67c
bazhuayu <297460306@qq.com>
2018-09-19 18:07:56 +0800

    添加查询所有订单列表云函数

--- miniprogram/pages/myorder/myorder.js
+++ miniprogram/pages/myorder/myorder.js
@@ -2,47 +2,29 @@
 const app = getApp()
 const db = wx.cloud.database()
 
+var openid = ''
 Page({
 
   data: {
-    openid: ''
+
   },
 
   onLoad: function (options) {
-    this.setData({
-      openid: getApp().globalData.openid
-    })
+        openid = getApp().globalData.openid
 
     this.queryAllOrder();
   },
 
   queryAllOrder: function () {
-    const cloud = require('../../wx-server-sdk')
-    cloud.init()
-    const MAX_LIMIT = 100
-    exports.main = async (event, context) => {
-      // 先取出集合记录总数
-      const countResult = await db.collection('subscribe').count()
-      const total = countResult.total
-      // 计算需分几次取
-      const batchTimes = Math.ceil(total / 100)
-      // 承载所有读操作的 promise 的数组
-      const tasks = []
-      for (let i = 0; i < batchTimes; i++) {
-        const promise = db.collection('subscribe').skip(i * MAX_LIMIT).limit(MAX_LIMIT).where({
-          _openid: this.data.openid
-        })
-          .get()
-        tasks.push(promise)
-      }
-      // 等待所有
-      return (await Promise.all(tasks)).reduce((acc, cur) => {
-        return {
-          data: acc.data.concat(cur.data),
-          errMsg: acc.errMsg,
-        }
-      })
-    }
+    wx.cloud.callFunction({
+      // 云函数名称
+      name: 'getAllOrder',
+      // 传给云函数的参数
+      success: function (res) {
+        console.log(res.result) 
+      },
+      fail: console.error
+    })
 
   }
 })
