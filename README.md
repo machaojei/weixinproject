2f02eb7e20f78db4b342050510f2f31f72e161d4
bazhuayu <bazhuayu@2dfire.com>
2018-12-23 21:23:05 +0800

    添加小程序码

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # weixinproject
 微信小程序开发练习项目
 
+![qrcode](qrcode.jpg)
--- cloudfunctions/getAllPicturePath/index.js
+++ cloudfunctions/getAllPicturePath/index.js
@@ -23,9 +23,7 @@ exports.main = async (event, context) => {
   // 承载所有读操作的 promise 的数组
   const tasks = []
   for (let i = 0; i < batchTimes; i++) {
-    const promise = db.collection('picture').skip(i * MAX_LIMIT).limit(MAX_LIMIT).where({
-      _openid: openid,
-    })
+    const promise = db.collection('picture').skip(i * MAX_LIMIT).limit(MAX_LIMIT)
       .get()
     tasks.push(promise)
   }
